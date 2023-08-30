656b7a7c77d07ea87afb367716fdeed2da4332e6
Kenjitsu <ricorga_97@hotmail.com>
2023-08-29 22:55:12 -0500

    Adding error handling to the API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using API.DTOs;
+﻿using API.Controllers;
+using API.DTOs;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -7,9 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API;
 
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController : ControllerBase
+public class ProductsController : BaseApiController
 {
 	private readonly IGenericRepository<Product> _productRepository;
 	private readonly IGenericRepository<ProductBrand> _productBrandRepository;
@@ -35,10 +35,14 @@ public class ProductsController : ControllerBase
 	}
 
 	[HttpGet("{id}")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
 	public async Task<ActionResult<ProductToReturnDTO>> GetProduct(int id)
 	{
 		var specification = new ProductsWithTypesAndBrandsSpecification(id);
 		var product = await _productRepository.GetEntityWithSpecifications(specification);
+
+		if (product == null) return NotFound(new ApiResponse(404));
 
 		return _mapper.Map<Product, ProductToReturnDTO>(product);
 	}
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
+using API.Errors;
+using API.Extensions;
+using API.Middlewares;
 using Core.Interfaces;
 using Infrastructure.Data;
-using Infrastructure.Data.Repositories;
 using Infrastructure.Data.SeedData;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,25 +11,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(options =>
-{
-	options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseStaticFiles();
 
