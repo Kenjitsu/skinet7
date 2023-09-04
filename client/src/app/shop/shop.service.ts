2044aedd127a782aa3b7b9591c558b211d2890f2
Ricardo Corredor <ricorga_97@hotmail.com>
2023-09-04 00:38:54 -0500

    Adding product details page, routing and lazy loading

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -29,6 +29,10 @@ export class ShopService {
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params})
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id)
+  }
+
   getTypes() {
     return this.http.get<Type[]>(this.baseUrl + 'products/types')
   }
